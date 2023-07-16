cc33a5a91a71bdb58448dfd0c2e795680ded66e7
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2023-07-16 13:47:58 +0530

    Update deep.py

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -1,1 +1,11 @@
+question = (input("What is the Answer to the Great Question of Life, the Universe, and Everything?"))
+question = question.lower()
+if question == "42":
+    print("yes")
+elif question == "forty two":
+    print("yes")
+elif question == "forty-two":
+    print("yes")
+else:
+    print("no")    
 
