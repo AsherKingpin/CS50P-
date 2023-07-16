9d8701cfba2cef84f5048b70d5e4c3c84f2b6821
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2023-07-16 13:10:42 +0530

    Create faces.py

---
+++ Problem Set 0/faces.py
@@ -0,0 +1,15 @@
+def convert(string):
+    if string == ":)":
+            converted_string = string.replace(":)","😊")
+            return converted_string
+    if string == ":(":
+           converted_string = string.replace(":(","😢")
+           return converted_string
+    
+def main():
+        string = input("enter the emoticon:")
+        if string == ":)":
+                print("the emoji that you wanted is ",convert(string))
+        if string == ":(":
+                print("the emoji that you wanted is",convert(string) )  
+main()
