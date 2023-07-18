9ed7324d6e4bd8f541ca29b42a09503fef9e7ee2
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2023-07-18 15:34:57 +0530

    Update interpreter.py

--- Problem Set 1/interpreter.py
+++ Problem Set 1/interpreter.py
@@ -1,1 +1,16 @@
+expression = (input("Expression:").strip())
+x, y, z = expression.split(" ")
+x = float(x)
+z = float(z)
+if y == "+":
+    total = (x+z)
+elif y == "/":
+    total = (x/z)
+elif y == "*":
+    total = (x*z)
+elif y == "-":
+    total = (x-z)
+else:
+    print("invaild operator")
 
+print(f"{total:1.f}")
