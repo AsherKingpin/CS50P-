83be87ec73ea0dcbfd760492324ecd528fd96a42
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2023-07-21 15:17:18 +0530

    Update Camel.py

--- Problem Set 2/Camel.py
+++ Problem Set 2/Camel.py
@@ -1,1 +1,19 @@
+def camel_to_snake(camel_case):
+    snake_case = ''
+    for char in camel_case:
+        if char.isupper():
+            snake_case += '_' + char.lower()
+        else:
+            snake_case += char
+    return snake_case
+
+def main():
+    print("Enter a variable name in camel case:")
+    camel_case_input = input()
+
+    snake_case_output = camel_to_snake(camel_case_input)
+    print("The variable name in snake case is:", snake_case_output)
+
+if __name__ == "__main__":
+    main()
 
