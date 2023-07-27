05120344c337b1c0c748ca73f5082946a0dcc527
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2023-07-27 16:45:38 +0530

    Update grocery.py

--- Problem Set 3/grocery.py
+++ Problem Set 3/grocery.py
@@ -1,1 +1,17 @@
+grocery = {
+
+}
+try:
+    while True:
+        item = input("Enter the items:").strip().title().upper()
+        if item in grocery:
+             grocery[item] += 1
+        else :
+             grocery[item] = 1
+except EOFError:
+         print("\nYour grocery list:")
+         for item, count in sorted(grocery.items()):
+            print(f"{count} {item}")
+except KeyError:
+     pass
 
